795274437264c816a3266b9dada4ea6cee7c8812
aleksei.bogdanov <aleksei.bogdanov@ringcentral.com>
2022-03-15 19:09:20 +0300

    Fixed coverage

---
+++ tools/coverage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+export RUSTFLAGS="-Zinstrument-coverage"
+export LLVM_PROFILE_FILE="coverage-%p-%m.profraw"
+rustup default nightly
+cargo +nightly install grcov
+rustup component add llvm-tools-preview
+brew install lcov
+
+cargo test --all --tests --no-fail-fast -- -Z unstable-options
+grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing  --ignore "*debug*"  --ignore "*cargo*" -o ./coverage/
+grcov . --binary-path ./target/debug/ -s . -t lcov --llvm --branch --ignore-not-existing --ignore "*debug*" --ignore "*cargo*" -o coverage.lcov
+lcov --summary coverage.lcov
