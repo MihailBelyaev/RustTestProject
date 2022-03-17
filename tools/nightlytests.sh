7bb9481093a54fff84730d18b26f6994e5cf8419
Mikhail Belyaev <belyaevmihail@mail.ru>
2022-03-17 15:57:48 +0300

    testreport

---
+++ tools/nightlytests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+export RUSTFLAGS="-Zinstrument-coverage"
+export LLVM_PROFILE_FILE="coverage-%p-%m.profraw"
+rustup default nightly-2021-10-21
+rustup toolchain list
+cargo +nightly-2021-10-21 install cargo2junit
+cargo test --all --tests --no-fail-fast -- -Z unstable-options --format json | cargo2junit > results.xml
